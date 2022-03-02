2d4cfe685b30b17e7f3fd56c0e6170224ef38be3
mloweedgar <mloweedgar0@gmail.com>
2022-03-02 11:56:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './todo-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="todo-app">
+
+        {/* Header start */}
+        <div id="header">
+            <img src={logo} alt="todo logo" />
+            <ul>
+                <li>Home</li>
+                <li>Menu_item_1</li>
+                <li>Menu_item_2</li>
+                <li>Disabled</li>
+            </ul>
+        </div>
+        {/* Header end */}
+
+
+        {/* Content start */}
+        <div id="contents">
+
+
+            <div id="todo-title">
+                <h2>Todo List</h2>
+            </div>
+            
+            {/* Create todo form */}
+            <div>
+                <form id="todo-form">
+                    <div class="todo-form-item">
+                        <label>Task</label>
+                        <input type="text" />
+                    </div>
+
+                    <div class="todo-form-item">
+                        <label>Description</label>
+                        <textarea rows="5"></textarea>
+                    </div>
+
+                    <input type="submit" value="Add Item" />
+
+
+                </form>
+
+            </div>
+            {/* Create todo form end */}
+
+            {/* Todo list Table start */}
+            <div id="todo-list-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>S.no</th>
+                            <th>Task</th>
+                            <th>Description</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>gh</td>
+                            <td>bhj</td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                       
+                        <tr>
+                            <td>2</td>
+                            <td>Free Freshness</td>
+                            <td>gvvvvvkvjvjk</td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            {/* Todo list Table end */}
+
+        </div>
+        {/* Content end */}
     </div>
+
   );
 }
 
